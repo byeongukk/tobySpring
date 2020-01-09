735d135b4f013d4dea0834a8d800abf88e82e60b
byeongukk <cbuk92@gmail.com>
2020-01-09 23:17:25 +0900

    1-5

--- src/main/java/com/home/toby/e1/example5/DaoFactory.java
+++ src/main/java/com/home/toby/e1/example5/DaoFactory.java
@@ -1,14 +1,30 @@
 package com.home.toby.e1.example5;
 
-//userDao의 생성 책임을 맡은 팩토리 클래스
-public class DaoFactory {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //빈 팩토리를 위한 오브젝트 설정을 담당하는 클래스 애노테이션
+public class DaoFactory {
+	/*
+	 * 1.5.1
+	 * @Configuration 
+	 * -> 빈 팩토리를 위한 오브젝트 설정을 담당하는 클래스로 선언
+	 * 
+	 * @Bean
+	 * -> 오브젝트를 생성하고 초기화 하는 메소드에 붙여줌
+	 * 
+	 * 이 두 가지 애노테이션으로 스프링에서 빈 팩토리 또는 애플리케이션 컨텍스트가
+	 * IoC 방식의 기능을 제공할때 사용할 설정정보가 됨
+	 * 
+	 * Java코드로 작성되었지만, XML과 같은 스프링 전용 설정 정보라 보는 것이 좋음.
+	 * */
+	@Bean
 	public UserDao userDao() {
-		//userDao 타입의 오브젝트를 어떻게 만들고 어떻게 준비시킬지 결정
 		return new UserDao(connectionMaker());
 		
 	}
 	
+	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker();
 	}
--- src/main/java/com/home/toby/e1/example5/UserDaoTest.java
+++ src/main/java/com/home/toby/e1/example5/UserDaoTest.java
@@ -2,12 +2,32 @@ package com.home.toby.e1.example5;
 
 import java.sql.SQLException;
 
-//테스트를 위한 mainClass
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
 public class UserDaoTest {
-	
+	/* 
+	 * 1.5.1
+	 * 
+	 * 애플리케이션 컨텍스트는 ApplicationContext 타입의 오브젝트
+	 * @Configuration이 붙은 java코드를 설정정보로 사용하라면,
+	 * AnnotationConfigApplicationContext 클래스를 이용
+	 * 
+	 * getBean으로 Bean으로 설정된 객체를 가져올 수 있음
+	 * (@Bean 이 붙은 메소드이름 / 해당 class)
+	 * 
+	 * Bean을 생성하는 방식이나 구성을 다르게 하기 위함
+	 * 
+	 * */
 	public static void main(String args[]) throws ClassNotFoundException, SQLException {
 		
-		UserDao dao = new DaoFactory().userDao();
+		//애플리케이션 컨텍스트 생성
+		ApplicationContext context 
+					= new AnnotationConfigApplicationContext(DaoFactory.class);
+		
+		//-> 결국에는 UserDao class를 알아야 된다??
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("uukk");
